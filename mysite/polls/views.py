0c30d55c2ffd918a0f604fa8f81fd043dc14049b
TomaSbert <johndoe@example.com>
2024-04-12 14:59:37 -0300

    [Parte 4.0.1] Archivo refactorizado (eliminacion de funciones)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -27,20 +27,6 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
@@ -62,7 +48,3 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
